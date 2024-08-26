9c160133a3403b7a846efc2fdbe81b0aef442a07
kdh30806 <kdh30806@naver.com>
2024-08-26 20:19:03 +0900

    네이버 maps api 추가(국외는 검색 안되는 이슈가 있음)

--- App.tsx
+++ App.tsx
@@ -10,14 +10,27 @@ import {
   Text,
   View,
 } from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const SCREENT_WIDTH = Dimensions.get('window').width;
 const API_KEY = `54ab9ddbcc5f748a72945bcf5dcbe455`;
+const CLIENT_ID = `2fq2aw8r15`;
+const CLIENT_SECRET = `YzfpLi67LLQxcHmbEhdUiTx2B8aDiguZiYjNIlMO`;
+const icons = {
+  Clouds: 'cloudy',
+  Clear: 'day-sunny',
+  Snow: 'snow',
+  Rain: 'rains',
+  Drizzle: 'rain',
+  Thunderstorm: 'lightning',
+  Atmosphere: 'cloudy-gusts',
+};
 
 const App = () => {
   const [latitude, setLatitude] = useState(0);
   const [logitude, setLogitude] = useState(0);
   const [days, setDays] = useState([]);
+  const [city, setCity] = useState('');
 
   useEffect(() => {
     if (Platform.OS === 'android') {
@@ -45,6 +58,19 @@ const App = () => {
               }),
             ),
           );
+        fetch(
+          `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=129.0226,35.1562&orders=legalcode&output=json`,
+          {
+            headers: {
+              'X-NCP-APIGW-API-KEY-ID': CLIENT_ID,
+              'X-NCP-APIGW-API-KEY': CLIENT_SECRET,
+            },
+          },
+        )
+          .then(res => res.json())
+          .then(data => {
+            setCity(data.results[0].region.area3.name);
+          });
       },
       error => {
         console.log(error);
@@ -65,9 +91,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>
-          서울({latitude}, {logitude})
-        </Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -85,9 +109,22 @@ const App = () => {
         ) : (
           days.map((day, index) => (
             <View style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.main.temp).toFixed(1)}
-              </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  width: '100%',
+                  justifyContent: 'space-between',
+                }}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -112,21 +149,28 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 68,
     fontWeight: '600',
+    color: 'white',
   },
   day: {
     width: SCREENT_WIDTH,
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
+    color: 'white',
+    marginBottom: 10,
   },
   description: {
     marginTop: -30,
-    fontSize: 60,
+    fontSize: 30,
+    color: 'white',
   },
   tinyText: {
     fontSize: 20,
+    color: 'white',
   },
 });
 
